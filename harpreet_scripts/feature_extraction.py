29fc20d128db5ae995f1cddb6d9a9c76509f0058
Harpreet Bargota <harpreet.bargota@agr.gc.ca>
2024-05-13 17:07:15 +0000

    Replace feature_extraction.py

--- harpreet_scripts/feature_extraction.py
+++ harpreet_scripts/feature_extraction.py
@@ -1,5 +1,15 @@
 
-################################################################################################################################
+
+
+
+
+import argparse
+import os
+import pandas as pd
+import cv2 
+import numpy as np
+
+
 ###Image classification pipeline using amg SAM, inverted mask, label, feature extraction and classification
 
     
@@ -36,7 +46,17 @@ sys.path.append("..")
 
 
 
+#Dependencies for SAM
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+sam_checkpoint = "sam_vit_h_4b8939.pth"
+model_type = "vit_h"
+device = "cuda"    
 
+
+
+
+
+## defining parameters for masks
 def show_anns(anns):
     if len(anns) == 0:
         return
@@ -52,140 +72,162 @@ def show_anns(anns):
         img[m] = color_mask
     ax.imshow(img)
 
-
-#Dependencies for SAM
-from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
-sam_checkpoint = "sam_vit_h_4b8939.pth"
-model_type = "vit_h"
-device = "cuda"    
-
-        
-#Pics = ['SAM/Faba-Seed-CC_Vf1-1-2.JPG','SAM/Faba-Seed-CC_Vf80-1-1.JPG','SAM/Faba-Seed-CC_Vf66-1-1.JPG','SAM/Faba-Seed-CC_Vf112-1-2.JPG','SAM/Faba-Seed-CC_Vf125-1-3.JPG','SAM/Faba-Seed-CC_Vf140-1-1.JPG','SAM/Faba-Seed-CC_Vf216-1-1.JPG','SAM/Faba-Seed-CC_Vf238-1-1.JPG','SAM/Faba-Seed-CC_Vf427-1-1.JPG','SAM/Faba-Seed-CC_Vf462-3-1.JPG' ]
-Pics = ['SAM/Faba-Seed-CC_Vf125-1-3.JPG','SAM/Faba-Seed-CC_Vf80-1-1.JPG', 'SAM/Faba-Seed-CC_Vf62-1-1.JPG']
-
-df_total = []
-
-#Step1:Read an image
-for x in Pics:
-	image = cv2.imread(x)   # read an image using openCV
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)   # convert into RGB format
-	image = crop(image, ((2000, 800), (50, 50), (0,0)), copy=False)   #crop the rectangular part  having color card 
+# defining the function for processing of images
 
 
-#Step 2: Image Segmenation using SegmentAnything MetaAI
+def process_images(input_folder, output_folder):
+    # Check if the input folder exists
+	if not os.path.exists(input_folder):
+		print(f"Error: Input folder '{input_folder}' does not exist.")
+		return
 
-	sam = sam_model_registry[model_type](checkpoint=sam_checkpoint) #SAM model and checkpoint
-	sam.to(device=device) #device
-	mask_generator = SamAutomaticMaskGenerator(sam, box_nms_thresh = 0.1, output_mode = "binary_mask") #Specify the parameters
-	masks_SAM = mask_generator.generate(image)  #generates masks
+    # Create the output folder if it doesn't exist
+	if not os.path.exists(output_folder):
+		os.makedirs(output_folder)
+		print(f"Output folder '{output_folder}' created.")
 
-	print("The total length of all masks is: ", len(masks_SAM))
-	print(masks_SAM[0].keys())
-
-# Metafile for SAM masks
-  
-	df1=[]
-	header1=['id','area','bbox_x0','bbox_y0','bbox_w','bbox_h','point_input_x','point_input_y','predicted_iou','stability_score','crop_box_x0','crop_box_y0','crop_box_w','crop_box_h']
-	metadata = []
-	for i, mask_data in enumerate(masks_SAM):
-		mask = mask_data["segmentation"]
-		#filename = f"binarymask-{i}.png"
-		#cv2.imwrite(filename, mask * 255)
-		mask_metadata = [
-            	str(i),
-            	str(mask_data["area"]),
-            	*[str(x) for x in mask_data["bbox"]],
-           	*[str(x) for x in mask_data["point_coords"][0]],
-            	str(mask_data["predicted_iou"]),
-            	str(mask_data["stability_score"]),
-            	*[str(x) for x in mask_data["crop_box"]],
-        	]    #create a list of metadata with header1
-		df=pd.DataFrame(mask_metadata, index=header1) #create the dataframe with header1 as index
-		df1.append(df) #append all the dfs for all the masks for an image
-	df2=pd.concat(df1,axis=1)  # join all the dataframes formed for all images
-	df_metadata=df2.T.apply(pd.to_numeric).set_index('id')  #change the datatype to numerical and set the index by 'id'for analysis
-
-	# Filtering the masks of beans and eliminatingg the masks of labels and coin
-	df_metadata = df_metadata.drop(df_metadata[(df_metadata['bbox_x0'] <= 1600) & (df_metadata['bbox_y0'] >= 2600)].index) #eliminating the masks for label
-	df_metadata = df_metadata.drop(df_metadata[(df_metadata['bbox_x0'] >= 3300) & (df_metadata['bbox_y0'] >= 2500)].index)  #eliminating the masks for coin
-	df_metadata = df_metadata.drop(df_metadata[(df_metadata['bbox_h'] >= 800)].index) #eliminating the masks that exceed a certain height of the bounding boxes 
-	df_metadata=df_metadata.loc[(df_metadata['area'] >= 5000) & (df_metadata['area'] <= 290000)] #filtering the masks according to the minimum and maximum area of beans
-	print (df_metadata)
-	#df_metdata.to_excel("SAM/metadata.xlsx")
-	Total_number_of_seeds=len(df_metadata.index)
-	print("Total number of seeds is ", Total_number_of_seeds)
-
-#print ("Filtering of mask is done")
-
-	Masks_list=df_metadata.index.tolist()
-	masks_SAM = [masks_SAM[x] for x in Masks_list]
-	#print(len(masks_SAM))
-	plt.figure()
-	plt.imshow(image)
-	show_anns(masks_SAM)
-	plt.axis('on')
-	plt.savefig("SAM/Mask_{x}.jpg")
-#plt.savefig("SAM/df_allmask{i}.png".format(i=i))  
-	print("After filtering of the masks, now the length of all masks required for feature extraction is: ", len(masks_SAM))
-##### IMAGE ANNOTATION --  Supervision detections
-	detections = sv.Detections.from_sam(sam_result=masks_SAM)
-	polygon_annotator = sv.PolygonAnnotator(color=sv.ColorPalette.ROBOFLOW, thickness=36, color_lookup=sv.ColorLookup.INDEX)
-	annotated_frame = polygon_annotator.annotate(scene=image.copy(),	detections=detections)
-	cv2.imwrite("SAM/annotated_frame{x}.jpg", annotated_frame)
-
-
-############################################################################################################################################
-############ FEATURE EXTRACTION
-
-	df_list=[]
-	for i, mask_data in enumerate(masks_SAM):
-		mask = mask_data["segmentation"]
- 	#label masks
-		label_image = measure.label(mask)
-
-	#analyse masks
-		properties = measure.regionprops(label_image, intensity_image=image)
-		regions=regionprops(label_image)
-# extract relevant properties
-		statistics = {
-		'Area':       [p.area for p in properties],
-      		'Perimeter':       [p.perimeter     for p in properties],
-	    	'MajorAxisLength': [p.major_axis_length  for p in properties],
-		'MinorAxisLength': [p.minor_axis_length for p in properties],
-		'ConvexArea': [p.area_convex  for p in properties],
-		'EquivDiameter': [p.equivalent_diameter_area for p in properties],
-		'Extent': [p.extent for p in properties],
-		'Solidity': [p.solidity for p in properties],
-		'Eccentricity': [p.eccentricity for p in properties]}
-
-  # convert to dataframe
-		df_FE = pd.DataFrame(statistics)
-
-		df_FE["AspectRation"] = df_FE["MajorAxisLength"]/df_FE['MinorAxisLength']
-		#df_FE["Roundness"]=df_FE[(4*3.14*(df_FE["Area"]))/((df_FE["Perimeter"])*(df_FE["Perimeter"]))]
-		df_FE["Compactness"] = df_FE["EquivDiameter"]/df_FE["MajorAxisLength"]
-		#Shape features
-		df_FE["Shapefactor1"] = df_FE["MajorAxisLength"]/df_FE["Area"]
-		df_FE["Shapefactor2"] = df_FE["MinorAxisLength"]/df_FE["Area"]
- 		#df_FE["Shapefactor3"] = df_FE["axis_minor_length"]/df_FE["area"]
- 		#df_FE["Shapefactor4"] = df_FE["axis_minor_length"]/df_FE["area"]
-		class_in_image=(x).split('.JPG')[0]
-		df_FE["class"]= (x).split('.JPG')[0]
-		df_list.append(df_FE)
-	df_FE2 = pd.concat(df_list)
-	df_total.append(df_FE2)
-df_image=pd.concat(df_total)
-print(df_image)
-	
-df_image.to_excel("SAM/Feature_Extraction.xlsx")   
-print ("FE done!") 
+	for root, dirs, files in os.walk(input_folder):
+		df_total = []
+		
+		for idx, file in enumerate(files):
+        # Read an image
+			image_path=os.path.join(root,file)
+			image_path = os.path.join(root,file)
+			image=cv2.imread(image_path)
+			image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)   
+			#image = crop(image, ((2000, 800), (50, 50), (0,0)), copy=False)   
+			image = crop(image, ((2200, 800), (50, 50), (0,0)), copy=False)
+	#Apply SegmentAnything model 
  
+			sam = sam_model_registry[model_type](checkpoint=sam_checkpoint) 
+			sam.to(device=device) 
+			mask_generator = SamAutomaticMaskGenerator(sam, box_nms_thresh = 0.1, output_mode = "binary_mask") 
+			masks_SAM = mask_generator.generate(image)  
+			print("The total length of all masks is: ", len(masks_SAM))
+			print(masks_SAM[0].keys())
+			
+            # Metadata
+			
+			df1=[]
+			header1=['id','area','bbox_x0','bbox_y0','bbox_w','bbox_h','point_input_x','point_input_y','predicted_iou','stability_score','crop_box_x0','crop_box_y0','crop_box_w','crop_box_h']
+			metadata = []
+			for i, mask_data in enumerate(masks_SAM):
+				mask = mask_data["segmentation"]
+				mask_metadata = [
+					str(i),
+					str(mask_data["area"]),
+					*[str(x) for x in mask_data["bbox"]],
+					*[str(x) for x in mask_data["point_coords"][0]],
+					str(mask_data["predicted_iou"]),
+					str(mask_data["stability_score"]),
+					*[str(x) for x in mask_data["crop_box"]],
+					]    #create a list of metadata with header1
+				df=pd.DataFrame(mask_metadata, index=header1)
+				df1.append(df)
+			df2=pd.concat(df1,axis=1)
+			df_metadata=df2.T.apply(pd.to_numeric).set_index('id')
+			
+            
+            # Filtering the masks of beans and eliminating the masks of labels and coin
+			df_metadata = df_metadata.drop(df_metadata[(df_metadata['bbox_x0'] <= 1600) & (df_metadata['bbox_y0'] >= 2500)].index) 
+			df_metadata = df_metadata.drop(df_metadata[(df_metadata['bbox_x0'] >= 3300) & (df_metadata['bbox_y0'] >= 2000)].index)
+			df_metadata = df_metadata.drop(df_metadata[(df_metadata['bbox_h'] >= 800)].index)
+			df_metadata = df_metadata.drop(df_metadata[(df_metadata['bbox_w'] >= 1000)].index)
+			df_metadata=df_metadata.loc[(df_metadata['area'] >= 5000) & (df_metadata['area'] <= 290000)]
+			print (df_metadata)
+			
+             
+	
+			image_filename = os.path.basename(image_path)
+			output_filename = f"Metadata_{image_filename}.xlsx"
+			output_path= os.path.join(output_folder, output_filename)
+			df_metadata.to_excel(output_path)
+			Total_number_of_seeds=len(df_metadata.index)
+			print("Total number of seeds is ", Total_number_of_seeds)
+			Masks_list=df_metadata.index.tolist()
+			masks_SAM=[masks_SAM[x] for x in Masks_list]
+		
+            
 
-      
+				
+			# Save fava bean masks in output_folder
+			for i, mask_data in enumerate(masks_SAM):
+				mask = mask_data["segmentation"]
+				image_filename = os.path.basename(image_path)
+				output_filename = f"{image_filename}_mask_{idx}_{i}.png"
+				output_path= os.path.join(output_folder, output_filename)
+				cv2.imwrite(output_path, mask*255)
+			
+			print("After filtering of the masks, now the length of all masks required for feature extraction is: ", len(masks_SAM))
+			
+            ##### IMAGE ANNOTATION --  Supervision detections
+			detections = sv.Detections.from_sam(sam_result=masks_SAM)
+			polygon_annotator = sv.PolygonAnnotator(color=sv.ColorPalette.ROBOFLOW, thickness=36, color_lookup=sv.ColorLookup.INDEX)
+			annotated_frame = polygon_annotator.annotate(scene=image.copy(),	detections=detections)
+			image_filename = os.path.basename(image_path)
+			output_filename = f"Annotated_image_{image_filename}.png"
+			output_path= os.path.join(output_folder, output_filename)
+			cv2.imwrite(output_path, annotated_frame)
+			
+# FEATURE EXTRACTION
+			df_list=[]
+			for i, mask_data in enumerate(masks_SAM):
+				mask = mask_data["segmentation"]
+				label_image = measure.label(mask)
+				#analyse masks
+				properties = measure.regionprops(label_image, intensity_image=image)
+				regions=regionprops(label_image)
+				# extract relevant properties
+				statistics = {
+		           'Area': [p.area for p in properties],
+      		       'Perimeter': [p.perimeter     for p in properties],
+	    	        'MajorAxisLength': [p.major_axis_length  for p in properties],
+		            'MinorAxisLength': [p.minor_axis_length for p in properties],
+		            'ConvexArea': [p.area_convex  for p in properties],
+		            'EquivDiameter': [p.equivalent_diameter_area for p in properties],
+		            'Extent': [p.extent for p in properties],
+		            'Solidity': [p.solidity for p in properties],
+		            'Eccentricity': [p.eccentricity for p in properties]}
+				# convert to dataframe
+				df_FE = pd.DataFrame(statistics)
+				df_FE["AspectRation"] = df_FE["MajorAxisLength"]/df_FE['MinorAxisLength']
+				#df_FE["Roundness"]=df_FE[(4*3.14*(df_FE["Area"]))/((df_FE["Perimeter"])*(df_FE["Perimeter"]))]
+				df_FE["Compactness"] = df_FE["EquivDiameter"]/df_FE["MajorAxisLength"]
+				#Shape features
+				df_FE["Shapefactor1"] = df_FE["MajorAxisLength"]/df_FE["Area"]
+				df_FE["Shapefactor2"] = df_FE["MinorAxisLength"]/df_FE["Area"]
+				#df_FE["Shapefactor3"] = df_FE["MinorAxisLength"]/df_FE["Area"]
+				#df_FE["Shapefactor4"] = df_FE["axis_minor_length"]/df_FE["area"]
+				class_in_image=(image_path).split('.JPG')[0]
+				df_FE["class"]= (image_path).split('.JPG')[0]
+				df_list.append(df_FE)
+			df_FE2 = pd.concat(df_list)
+			df_total.append(df_FE2)
+		df_image=pd.concat(df_total)
+		print(df_image)
+		output_filename = f"Fava_bean_Features_extraction.xlsx"
+		output_path= os.path.join(output_folder, output_filename)
+		df_image.to_excel(output_path)
+		print ("Feature extraction from fava bean images is completed..!")
 
 
 
-     
 
 
-    
+if __name__ == "__main__":
+    # Create ArgumentParser object
+    parser = argparse.ArgumentParser(description="Process files from input folder to output folder.")
+
+    # Add arguments for input and output folders
+    parser.add_argument("input_folder", help="Path to the input folder")
+    parser.add_argument("output_folder", help="Path to the output folder")
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Call the process_folders function with provided input and output folders
+    process_images(args.input_folder, args.output_folder)
+
+#for x in args.input_folder:
+#	image = cv2.imread(r'x')   # read an image using openCV
+
